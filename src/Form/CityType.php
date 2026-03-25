5e32d11bba09ed3086143d910761591846e19bba
mseigneur2025 <mael.seigneur2025@campus-eni.fr>
2026-03-25 16:34:11 +0100

    Option création de lieu ajoutée + unversionned files

---
+++ src/Form/CityType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la ville'
+            ])
+            ->add('zipcode', TextType::class, [
+                'label' => 'Code postal'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => City::class,
+        ]);
+    }
+}
