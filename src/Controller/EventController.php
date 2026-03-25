8fa155a0035c00bfbf829774268741a113bd89c2
mguedon2025 <mathilde.guedon2025@campus-eni.fr>
2026-03-25 16:56:35 +0100

    affichage liste évènements

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -16,10 +16,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('events', name: 'events_')]
 final class EventController extends AbstractController
 {
-    #[Route('', name: 'list', requirements: ['id' => '\d+'])]
+    #[Route('', name: 'list')]
     public function list(EventRepository $eventRepository): Response
     {
-        $events = $eventRepository->findAll();
+//        $events = $eventRepository->findAll();
+        $events = $eventRepository->findPublishedEventByDate();
         return $this->render('event/list.html.twig', [
             'events' => $events,
         ]);
