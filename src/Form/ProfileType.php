c700d7c2fcb7b755908be44b26c62bda3241e6de
mguedon2025 <mathilde.guedon2025@campus-eni.fr>
2026-03-25 15:20:12 +0100

    affichage de la photo de profil

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -9,12 +9,14 @@ use App\Repository\CampusRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ProfileType extends AbstractType
@@ -29,7 +31,7 @@ class ProfileType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe :'],
                 'second_options' => ['label' => 'Confirmation :'],
                 'invalid_message' => 'Both passwords do not match.',
-                'required' => true,
+                'required' => false,
                 'mapped' => false
             ])
             ->add('firstname', TextType::class, ['label' => 'Prénom :', 'required' => true, 'attr' => ['placeholder' => 'Ada']])
@@ -37,7 +39,7 @@ class ProfileType extends AbstractType
             ->add('phoneNb', TelType::class, ['label' => 'Telephone :', 'required' => false, 'attr' => ['placeholder' => '06 06 06 06 06']])
             ->add('email', EmailType::class, ['label' => 'Email :', 'required' => true, 'attr' => ['placeholder' => 'ada.lovelace@eni.fr']])
 //            ->add('active')
-            ->add('image', TextType::class, ['label' => 'Image :', 'required' => false])
+            ->add('image', FileType::class, ['mapped' => false, 'label' => 'Image de profil :', 'required' => false, 'constraints'=>[new Image(mimeTypes: ['image/jpeg', 'image/jpg', 'image/png', 'image/svg', 'image/webp', 'image/tiff'], minWidth: 20)]])
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
                 'choice_label' => 'name',
