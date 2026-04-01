3da933a764f5f9294903efa0eafaedd4be963c01
akabbashialiham2025 <mokashfikabashi@gmail.com>
2026-04-01 16:40:31 +0200

    Gerer les campus ok

--- src/Form/CampusType.php
+++ src/Form/CampusType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,9 +12,8 @@ class CampusType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', null, [
+            ->add('name', TextType::class, [
                 'label' => 'Nom de campus',
-                'required' => true,
             ]);
     }
     public function configureOptions(OptionsResolver $resolver): void
